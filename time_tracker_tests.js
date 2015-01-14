4af445edd59855189733485ccec51d6050836a9a
Jon Perl <perl.jonathan@gmail.com>
2015-01-13 23:15:11 -0500

    Make tests more succinct
    Fix travis by considering existing timeouts

--- time_tracker_tests.js
+++ time_tracker_tests.js
@@ -18,54 +18,40 @@ Meteor.setTimeout = function (func) {
   return timeoutId;
 };
 
-var testInvalidations = function (){
+var testTimeTracker = function (test, dependOnTime) {
+  var invalidations = 0;
 
+  var existingTimeouts = Object.keys(timeouts).length;
+
+  var handle = Tracker.autorun(function () {
+    dependOnTime();
+    invalidations++;
+  });
+
+  // Tracker should run once on startup
+  test.equal(invalidations, 1);
+
+  // After the timeout it should have run again
+  runTimeouts();
+  Tracker.flush();
+  test.equal(invalidations, 2);
+
+  // it should have registered another setTimeout
+  test.equal(Object.keys(timeouts).length - existingTimeouts, 1);
+  handle.stop();
+
+  // the timeout should be cleared on invalidation
+  test.equal(Object.keys(timeouts).length - existingTimeouts, 0);
 };
 
 Tinytest.add('time-tracker - changeIn', function (test) {
-  var invalidations = 0;
-
-  var handle = Tracker.autorun(function () {
+  testTimeTracker(test, function () {
     TimeTracker.changeIn(1000);
-    invalidations++;
   });
-
-  // Tracker should run once on startup
-  test.equal(invalidations, 1);
-
-  // After the timeout it should have run again
-  runTimeouts();
-  Tracker.flush();
-  test.equal(invalidations, 2);
-
-  // it should have registered another setTimeout
-  test.equal(Object.keys(timeouts).length, 1);
-  handle.stop();
-
-  // the timeout should be cleared on invalidation
-  test.equal(Object.keys(timeouts).length, 0);
 });
 
 Tinytest.add('time-tracker - changeAt', function (test) {
-  var invalidations = 0;
-
-  var handle = Tracker.autorun(function () {
+  testTimeTracker(test, function () {
     TimeTracker.changeAt(new Date());
-    invalidations++;
   });
-
-  // Tracker should run once on startup
-  test.equal(invalidations, 1);
-
-  // After the timeout it should have run again
-  runTimeouts();
-  Tracker.flush();
-  test.equal(invalidations, 2);
-
-  // it should have registered another setTimeout
-  test.equal(Object.keys(timeouts).length, 1);
-  handle.stop();
-
-  // the timeout should be cleared on invalidation
-  test.equal(Object.keys(timeouts).length, 0);
 });

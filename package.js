d8251f3bb2c2591d2ffcbc2640370a18c98a4fda
Morten Henriksen <mh@gi2.dk>
2015-02-01 19:22:21 +0100

    Bump to version 1.0.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dispatch:time-tracker',
   summary: 'Time-based reactivity.',
-  version: '1.0.1',
+  version: '1.0.2',
   git: 'https://github.com/DispatchMe/meteor-time-tracker.git'
 });
 
